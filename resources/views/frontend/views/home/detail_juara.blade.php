e2f791b07bf6a128221e7ac6d04c44c25bf62272
Syofian Irwanda <syofian.irwanda@iconpln.co.id>
2026-01-07 22:24:33 +0700

    ss

--- resources/views/frontend/views/home/detail_juara.blade.php
+++ resources/views/frontend/views/home/detail_juara.blade.php
@@ -41,28 +41,8 @@
                     <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0" style="cursor: pointer">
                         Juara
                     </h1>
-                    <!--end::Title-->
-                    <!--begin::Breadcrumb-->
-                    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
-                        <!--begin::Item-->
-                        <li class="breadcrumb-item text-muted">
-                            <a @click="changePage('dashboard')"  class="text-muted text-hover-primary"  style="cursor: pointer">
-                                Home
-                            </a>
-                        </li>
-                        <!--end::Item-->
-                        <!--begin::Item-->
-                        <li class="breadcrumb-item">
-                            <span class="bullet bg-gray-500 w-5px h-2px"></span>
-                        </li>
-                        <!--end::Item-->
-                        <!--begin::Item-->
-                        <li class="breadcrumb-item text-muted">
-                            {{$juara->title}}
-                        </li>
-                        <!--end::Item-->
-                    </ul>
-                    <!--end::Breadcrumb-->
+                  
+               
                 </div>
                 <!--end::Page title-->
             </div>
