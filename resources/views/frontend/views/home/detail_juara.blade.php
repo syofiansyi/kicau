42caf5c044596f5fdab13daec2eaca40463781ac
Syofian Irwanda <syofian.irwanda@iconpln.co.id>
2026-01-07 23:50:52 +0700

    s

--- resources/views/frontend/views/home/detail_juara.blade.php
+++ resources/views/frontend/views/home/detail_juara.blade.php
@@ -1,77 +1,77 @@
-
-
-
 @extends('frontend.includes.index')
+
+
 @section('title')
     Kopdar LovedBird Indonesia - Detail Juara
 @endsection
 @push('addon-style')
-    <link href="{{ asset('Frontend/css/style.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('Frontend/css/event_style.css') }}" rel="stylesheet" type="text/css" />
-    <style>
-    table {
-        caption-side: bottom;
-        border-collapse: collapse;
-        border: 1px solid #000 !important;
-    }
-
-    table th,
-    table td {
-        border: 1px solid #000 !important;
-        padding: 8px;
-        /* opsional: untuk spasi di dalam sel */
-    }
-
-</style>
+    <link href="{{ asset('Frontend/css/custome.css') }}" rel="stylesheet" type="text/css" />
 @endpush
 @section('meta')
     <meta property="og:type" content="article">
     <meta property="og:title" content="{{ $juara->title }}">
-    <meta property="og:image" content="{{ $juara->photo ? asset('Upload/juara/' . $juara->photo) : asset('Frontend/img/logo.png') }}">
+    <meta property="og:image"
+        content="{{ $juara->photo ? asset('Upload/juara/' . $juara->photo) : asset('Frontend/img/logo.png') }}">
     <meta property="og:url" content="{{ url()->current() }}">
 @endsection
 @section('main')
-    <div class="d-flex flex-column flex-column-fluid">
-        <div id="kt_app_toolbar" class="app-toolbar  py-3 py-lg-6 ">
-            <!--begin::Toolbar container-->
-            <div id="kt_app_toolbar_container" class="app-container  container-fluid d-flex flex-stack ">
-                <!--begin::Page title-->
-                <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 ">
-                    <!--begin::Title-->
-                    <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0" style="cursor: pointer">
-                        Juara
-                    </h1>
-                  
-               
-                </div>
-                <!--end::Page title-->
-            </div>
-            <!--end::Toolbar container-->
-        </div>
-        <!--begin::Content-->
-        <div id="kt_app_content" class="app-content flex-column-fluid">
-            <div class="app-container container-fluid">
-                <!--Evenet-->
-                <div class="container-fluid">
-                    <!-- Bagian utama -->
-                    <div class="d-flex flex-column  align-items-start pt-4">
-                        <!-- Gambar -->
-                        <div class="mb-3 mb-md-0 me-md-4">
-                            <img src="{{ asset('Upload/juara/' . $juara->photo) }}" alt="Event Image" class="img-fluid rounded shadow" />
-                        </div>
+    <div class="flex min-h-screen w-full">
 
-                        <!-- Keterangan -->
-                        <div>
-                            <h3 class="fw-bold">{{$juara->title}}</h3>
-                            <p class="text-description" style="text-align: justify;" v-html="dataBerita.detailBerita">
-                                {!! $juara->description !!}
-                            </p>
+        {{-- Konten Kiri 4/5 --}}
+        <div class="flex-shrink-0 w-4/5">
+            <div class="d-flex flex-column flex-column-fluid">
+                <div id="kt_app_toolbar" class="app-toolbar  py-3 py-lg-6 ">
+                    <!--begin::Toolbar container-->
+                    <div id="kt_app_toolbar_container" class="app-container  container-fluid d-flex flex-stack ">
+                        <!--begin::Page title-->
+                        <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 ">
+                            <!--begin::Title-->
+                            <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0"
+                                style="cursor: pointer">
+                                Tips & Trik
+                            </h1>
+                            <!--end::Title-->
+                            <!--begin::Breadcrumb-->
+                          
+                            <!--end::Breadcrumb-->
+                        </div>
+                        <!--end::Page title-->
+                    </div>
+                    <!--end::Toolbar container-->
+                </div>
+                <!--begin::Content-->
+                <div id="kt_app_content" class="app-content flex-column-fluid mt-10">
+                    <div class="app-container container-fluid">
+                        <!--Evenet-->
+                        <div class="container-fluid">
+                            <!-- Bagian utama -->
+                            <div class="d-flex flex-column flex-md-row align-items-start pt-4">
+                                <!-- Gambar -->
+                                <div class="mb-3 mb-md-0 me-md-4">
+                                    <img src="{{ asset('Upload/juara/' . $juara->photo) }}" alt="Tips Image"
+                                        class="img-fluid rounded shadow" />
+                                </div>
+
+                                <!-- Keterangan -->
+                                <div>
+                                    <h3 class="fw-bold">{{ $juara->title }}</h3>
+                                    <p class="" style="text-align: justify;">
+                                        {!! $juara->description !!}
+                                    </p>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
-                </div>
-            </div>
 
+                </div>
+                <!--end::Content-->
+            </div>
         </div>
-        <!--end::Content-->
+
+
+        @include('components.banner')
+
+
     </div>
 @endsection
--- resources/views/frontend/views/tips/detail_tips.blade.php
+++ resources/views/frontend/views/tips/detail_tips.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('title')
-    Kopdar LovedBird Indonesia - Tips & Trik
+    Kopdar LovedBird Indonesia -  Tips & Trik
 @endsection
 @push('addon-style')
     <link href="{{ asset('Frontend/css/custome.css') }}" rel="stylesheet" type="text/css" />
@@ -15,72 +15,70 @@
     <meta property="og:url" content="{{ url()->current() }}">
 @endsection
 @section('main')
-    <div class="flex min-h-screen w-full">
-
-        {{-- Konten Kiri 4/5 --}}
-        <div class="flex-shrink-0 w-5/6">
-            <div class="d-flex flex-column flex-column-fluid">
-                <div id="kt_app_toolbar" class="app-toolbar  py-3 py-lg-6 ">
-                    <!--begin::Toolbar container-->
-                    <div id="kt_app_toolbar_container" class="app-container  container-fluid d-flex flex-stack ">
-                        <!--begin::Page title-->
-                        <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 ">
-                            <!--begin::Title-->
-                            <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0"
-                                style="cursor: pointer">
-                                Tips & Trik
-                            </h1>
-                            <!--end::Title-->
-                            <!--begin::Breadcrumb-->
-                            <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
-                                <!--begin::Item-->
-                                <li class="breadcrumb-item text-muted">
-                                    <a @click="changePage('tips')" class="text-muted text-hover-primary"
-                                        style="cursor: pointer">
-                                        Tips & Trik
-                                    </a>
-                                </li>
-                                <!--end::Item-->
-                                <!--begin::Item-->
-                                <li class="breadcrumb-item">
-                                    <span class="bullet bg-gray-500 w-5px h-2px"></span>
-                                </li>
-                                <!--end::Item-->
-                                <!--begin::Item-->
-                                <li class="breadcrumb-item text-muted">
-                                    Tips & Trik {{ $tips->title }}
-                                </li>
-                                <!--end::Item-->
-                            </ul>
-                            <!--end::Breadcrumb-->
-                        </div>
-                        <!--end::Page title-->
-                    </div>
-                    <!--end::Toolbar container-->
+ <div class="flex min-h-screen w-full">
+    
+    {{-- Konten Kiri 4/5 --}}
+    <div class="flex-shrink-0 w-4/5">
+       <div class="d-flex flex-column flex-column-fluid">
+        <div id="kt_app_toolbar" class="app-toolbar  py-3 py-lg-6 ">
+            <!--begin::Toolbar container-->
+            <div id="kt_app_toolbar_container" class="app-container  container-fluid d-flex flex-stack ">
+                <!--begin::Page title-->
+                <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 ">
+                    <!--begin::Title-->
+                    <h1 class="page-heading d-flex text-gray-900 fw-bold fs-3 flex-column justify-content-center my-0"
+                        style="cursor: pointer">
+                         Tips & Trik
+                    </h1>
+                    <!--end::Title-->
+                    <!--begin::Breadcrumb-->
+                    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">
+                            <a @click="changePage('tips')" class="text-muted text-hover-primary" style="cursor: pointer">
+                                 Tips & Trik
+                            </a>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item">
+                            <span class="bullet bg-gray-500 w-5px h-2px"></span>
+                        </li>
+                        <!--end::Item-->
+                        <!--begin::Item-->
+                        <li class="breadcrumb-item text-muted">
+                            Tips {{ $tips->title }}
+                        </li>
+                        <!--end::Item-->
+                    </ul>
+                    <!--end::Breadcrumb-->
                 </div>
-                <!--begin::Content-->
-                <div id="kt_app_content" class="app-content flex-column-fluid">
-                    <div class="app-container container-fluid">
-                        <!--Evenet-->
-                        <div class="container-fluid">
-                            <!-- Bagian utama -->
-                            <div class="d-flex flex-column flex-md-row align-items-start pt-4">
-                                <!-- Gambar -->
-                                <div class="mb-3 mb-md-0 me-md-4">
-                                    <img src="{{ asset('Upload/tips/' . $tips->photo) }}" alt="Tips Image"
-                                        class="img-fluid rounded shadow" />
-                                </div>
+                <!--end::Page title-->
+            </div>
+            <!--end::Toolbar container-->
+        </div>
+        <!--begin::Content-->
+        <div id="kt_app_content" class="app-content flex-column-fluid">
+            <div class="app-container container-fluid">
+                <!--Evenet-->
+                <div class="container-fluid">
+                    <!-- Bagian utama -->
+                    <div class="d-flex flex-column flex-md-row align-items-start pt-4">
+                        <!-- Gambar -->
+                        <div class="mb-3 mb-md-0 me-md-4">
+                            <img src="{{ asset('Upload/tips/' . $tips->photo) }}" alt="Tips Image"
+                                class="img-fluid rounded shadow" />
+                        </div>
 
-                                <!-- Keterangan -->
-                                <div>
-                                    <h3 class="fw-bold">{{ $tips->title }}</h3>
-                                    <p class="" style="text-align: justify;">
-                                        {!! $tips->description !!}
-                                    </p>
-                                </div>
-                            </div>
+                        <!-- Keterangan -->
+                        <div>
+                            <h3 class="fw-bold">{{ $tips->title }}</h3>
+                            <p class="" style="text-align: justify;">
+                                {!! $tips->description !!}
+                            </p>
                         </div>
                     </div>
+                   
                 </div>
             </div>
 
@@ -89,9 +87,10 @@
     </div>
     </div>
 
+   
+        @include('components.banner')
+   
 
-    @include('components.banner')
-
-
-    </div>
+</div>
+  
 @endsection
