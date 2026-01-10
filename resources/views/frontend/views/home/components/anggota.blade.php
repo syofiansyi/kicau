5275e368352cdbc9a0de2abe379a61ce70d5062b
Syofian Irwanda <syofian.irwanda@iconpln.co.id>
2026-01-10 22:38:36 +0700

    d

--- resources/views/frontend/views/home/components/anggota.blade.php
+++ resources/views/frontend/views/home/components/anggota.blade.php
@@ -45,18 +45,21 @@
                                             {{ $loop->iteration }}
                                         </td>
 
-                                        <td class="px-3 py-2">
+                                        <td class="px-3 py-2 max-w-[150px]">
                                             <div class="flex items-center gap-3">
                                                 <img src="{{ asset('Upload/anggota/' . $item->photo) }}"
                                                     alt="Foto Anggota"
                                                     class="w-[50px] h-[50px] object-cover rounded-md ring-1 ring-gray-300"
                                                     loading="lazy"
                                                     onerror="this.onerror=null;this.src='{{ asset('images/default-avatar.png') }}';">
-                                                <span class="font-medium whitespace-nowrap">
+                                                <span
+                                                    class="font-medium whitespace-nowrap overflow-hidden text-ellipsis max-w-[100px]"
+                                                    title="{{ $item->nama_burung }}">
                                                     {{ $item->nama_burung }}
                                                 </span>
                                             </div>
                                         </td>
+
 
                                         <td class="px-3 py-2 font-medium">
                                             {{ $item->nama_pemilik }}
@@ -97,6 +100,4 @@
     .table tbody tr:last-child td {
         border-bottom: none;
     }
-
-   
 </style>
